8f5cee639daf5efa6c848d73710404d5d38cfdad
sonicpro <chegnenado@yahoo.com>
2025-12-10 11:25:17 +0200

    Added CreateUser API.

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -3,6 +3,6 @@ import { PrismaService } from './prisma.service.js';
 
 @Module({
   providers: [PrismaService],
-  exports: [PrismaModule],
+  exports: [PrismaService],
 })
 export class PrismaModule {}
