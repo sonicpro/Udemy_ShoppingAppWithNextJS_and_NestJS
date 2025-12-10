768f28f332bf838e13e70dfc4a5b16bbada6cdb3
sonicpro <chegnenado@yahoo.com>
2025-12-10 15:00:57 +0200

    Fixed multipart/form-data payload for users controller POST API.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseInterceptors } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-user.request.js';
 import { UsersService } from './users.service.js';
+import { NoFilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
+  @UseInterceptors(NoFilesInterceptor())
   createUser(@Body() request: CreateUserRequest) {
     return this.usersService.createUser(request);
   }
