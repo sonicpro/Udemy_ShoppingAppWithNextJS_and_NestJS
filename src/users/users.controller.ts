bea1f842a39d8852864662cfe599136f15b9a9f1
sonicpro <chegnenado@yahoo.com>
2025-12-10 10:29:18 +0200

    Initial commit. Prisma module added.

---
+++ src/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateUserRequest } from './dto/create-user.request.js';
+import { UsersService } from './users.service.js';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  createUser(@Body() request: CreateUserRequest) {
+    return this.usersService.createUser(request);
+  }
+}
